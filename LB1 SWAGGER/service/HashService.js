4583b4ed422cd43e0b310c31d8af011d510ae2a6
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-02-19 08:09:16 +0300

    Now it works
    Type in JSON body raw as {"data": "message_to_hash"}

--- LB1 SWAGGER/service/HashService.js
+++ LB1 SWAGGER/service/HashService.js
@@ -6,11 +6,6 @@
  **/
 
 const crypto = require('crypto');
-//var sha256 = require('js-sha256').sha256;
-
-//sha256(res.json({requestBody: req.body}))
-//var crypt_message = sha256('hello world!');
-//var resultOf = request.body;
 
 exports.hashPOST = function(req) {
   return new Promise(function(resolve, reject) {
@@ -19,14 +14,11 @@ exports.hashPOST = function(req) {
   //console.log(data);
   var hash = crypto.createHash("sha256").update(data, "utf-8").digest("hex")
 
-  hash = hash.split("").map(function (ch){ return ch.charCodeAt(0).toString(16) }).join(" ")
-
   var response = {};
   response['application/json'] = {"hash": hash};
 
   if (Object.keys(response).length > 0) {
-    resolve(`HASH result - ${response[Object.keys(response)[0]]}`);
-    // HASH result - ${response[Object.keys(response)[0]]}
+    resolve(response[Object.keys(response)[0]]);
   } else { resolve("Empty resolve"); }
 
   });
