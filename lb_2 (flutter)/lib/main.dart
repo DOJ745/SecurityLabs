a9ea78a63742a5264460c918cc4ed1964c85bc20
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-04-05 11:53:46 +0300

    Cosmetic changes

--- lb_2 (flutter)/lib/main.dart
+++ lb_2 (flutter)/lib/main.dart
@@ -33,9 +33,9 @@ class MyFormState extends State {
       new Text('IP or DNS name:', style: TextStyle(fontSize: 20.0),),
 
       // ignore: missing_return
-      new TextFormField(validator: (value){
+      new TextFormField(validator: (value)
+      {
         if (value.isEmpty) return 'Enter IP!';
-
         try { ip = value.toString(); } catch(e) {
           ip = null;
           return e.toString();
@@ -85,7 +85,8 @@ class MyFormState extends State {
         }),
 
       new RaisedButton(onPressed: () async {
-        if(_formKey.currentState.validate()) Scaffold.of(context).showSnackBar(SnackBar(content: Text('Success'), backgroundColor: Colors.green,));
+        if(_formKey.currentState.validate()) Scaffold.of(context).showSnackBar(SnackBar(content: Text('Success'),
+          backgroundColor: Colors.green,));
 
         /*
         http.Response res = await http.post('https://' + ip + ":" + port + "/vhash",
@@ -111,7 +112,10 @@ class MyFormState extends State {
         print(reply);
 
         }, child: Text('Send'), color: Colors.blue, textColor: Colors.white,),
-    ],)));
+    ],
+    )
+    )
+    );
   }
 
 }
@@ -126,15 +130,6 @@ void main() => runApp(
     )
 );
 
-/*
-void main() {
-  runApp(MaterialApp(
-      home:  Scaffold(
-        body: TextField(),
-        appBar: AppBar(title: Text("LB2_3")),)
-  ));
-}*/
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -142,19 +137,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter LB2_3'),
@@ -164,15 +147,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -184,48 +158,17 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    setState(() { _counter++; });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -242,7 +185,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
